b426d287b9e73c178729bbaa0764baf562e04991
Mukesh Kharita <mukeshnithcse@gmail.com>
2017-08-22 15:19:22 +0530

    Map updated with icon

--- App/app/src/main/java/com/example/mukesh/marauders/MapsActivity.java
+++ App/app/src/main/java/com/example/mukesh/marauders/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.mukesh.marauders;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -10,12 +12,20 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Timer myTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +35,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        GpsTracker gt = new GpsTracker(getApplicationContext());
-        Location l = gt.getLocation();
-        if( l == null){
-            Toast.makeText(getApplicationContext(),"GPS unable to get Value",Toast.LENGTH_SHORT).show();
-        }else {
-            double lat = l.getLatitude();
-            double lon = l.getLongitude();
-            Toast.makeText(getApplicationContext(),"GPS Lat = "+lat+"\n lon = "+lon,Toast.LENGTH_SHORT).show();
-        }
+
+        myTimer = new Timer();
+        myTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                TimerMethod();
+            }
+
+        }, 0, 5000);
     }
+
+    private void TimerMethod()
+    {
+        //This method is called directly by the timer
+        //and runs in the same thread as the timer.
+
+        //We call the method that will work with the UI
+        //through the runOnUiThread method.
+        this.runOnUiThread(Timer_Tick);
+    }
+
+    private Runnable Timer_Tick = new Runnable() {
+        public void run() {
+            onMapReady(mMap);
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if(mMap != null)
+            mMap.clear();
+        LatLng location = null;
+        GpsTracker gt = new GpsTracker(getApplicationContext());
+        Location l = gt.getLocation();
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        List<Address> addresses = null;
+        Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+
+        if( l == null){
+            location = new LatLng(0, 0);
+            Toast.makeText(getApplicationContext(),"GPS unable to get Value",Toast.LENGTH_SHORT).show();
+        }else {
+            double lat = l.getLatitude();
+            double lon = l.getLongitude();
+            location = new LatLng(lat, lon);
+            try {
+                addresses  = geocoder.getFromLocation(lat,lon, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(getApplicationContext(),"GPS Lat = "+lat+"\n lon = "+lon,Toast.LENGTH_SHORT).show();
+        }
+        if(addresses.size() > 0)
+            mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.myloc)).position(location).title(addresses.get(0).getLocality()));
+        else
+            mMap.addMarker(new MarkerOptions().position(location).title("Not Available"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
     }
 }
 
